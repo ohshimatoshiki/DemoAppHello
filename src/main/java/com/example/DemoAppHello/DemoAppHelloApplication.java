bb0c5e4900d9784b42c3a036a38ba61b74c61d8b
Toshiki Ohshima <ohsima-toshiki-bg@ynu.jp>
2026-06-29 01:31:32 +0900

    Creare Spring Boot hello app

---
+++ src/main/java/com/example/DemoAppHello/DemoAppHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.DemoAppHello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAppHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAppHelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DemoAppHello/DemoAppHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DemoAppHello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
